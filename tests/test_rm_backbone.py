af38152d4a2cb950729463a56a2ab452220170dd
Zichen <lkevinzc@gmail.com>
2024-09-09 22:30:39 +0800

    work on rm

--- tests/test_rm_backbone.py
+++ tests/test_rm_backbone.py
@@ -1,10 +1,8 @@
-import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import pdb
 from typing import List
 
 import pandas as pd
+from fire import Fire
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoTokenizer
@@ -12,70 +10,84 @@ from transformers import AutoTokenizer
 from ellm.rm.backbone import DebertaV2PairRM
 from ellm.types import PreferenceData
 
-buffer = pd.read_pickle(
-    "/home/aiops/liuzc/ellm/output/online_SimPO_dump_0811T15:29/buffer.pkl"
-)
-data = []
-for b in buffer:
-    data += list(b)
-
-backbone = DebertaV2PairRM.from_pretrained(
-    "llm-blender/PairRM-hf", device_map="cuda:0"
-).eval()
-tokenizer = AutoTokenizer.from_pretrained("llm-blender/PairRM-hf")
+buffer_dict = {
+    "v1": "/home/aiops/liuzc/ellm/output/online_SimPO_dump_0811T15:29/buffer.pkl",
+    "v2": "/home/aiops/liuzc/ellm/output/bt_sample_dpo_online_passive_0907T06:48/all_buffer.pkl",
+}
 
 
-source_prefix = "<|source|>"
-cand_prefix = "<|candidate|>"
-max_length = 2048
-source_max_length = 1224
+def main(
+    model_name: str,
+    buffer_version: str = "v1",  # llm-blender/PairRM-hf
+    tag: str = "",
+):
+    buffer = pd.read_pickle(buffer_dict[buffer_version])
+    data = []
+    for b in buffer:
+        data += list(b)
 
+    _model_name = model_name.replace("/", "_")
+    out_name = f"rm_features_{buffer_version}_{_model_name}{tag}.pt"
+    print("output to ", out_name)
 
-def collate_fn(item_list: List[PreferenceData]):
-    prompt_ids = []
-    chosen_ids = []
-    rejected_ids = []
-    for pref in item_list:
-        source_ids = tokenizer.encode(
-            source_prefix + pref.prompt, max_length=source_max_length, truncation=True
+    backbone = DebertaV2PairRM.from_pretrained(model_name, device_map="cuda").eval()
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    max_length = 2048
+    source_max_length = 1224
+
+    # backbone.cand_prefix_id = backbone.config.cand1_prefix_id
+    # backbone.cand_prefix = "<candidate1>"
+
+    def collate_fn(item_list: List[PreferenceData]):
+        prompt_ids = []
+        chosen_ids = []
+        rejected_ids = []
+        for pref in item_list:
+            source_ids = tokenizer.encode(
+                backbone.source_prefix + pref.prompt,
+                max_length=source_max_length,
+                truncation=True,
+            )
+            candidate_max_length = max_length - len(source_ids)
+            chosen_candidate_ids = tokenizer.encode(
+                backbone.cand_prefix + pref.chosen_response,
+                max_length=candidate_max_length,
+                truncation=True,
+            )
+            rejected_candidate_ids = tokenizer.encode(
+                backbone.cand_prefix + pref.rejected_response,
+                max_length=candidate_max_length,
+                truncation=True,
+            )
+
+            prompt_ids.append(source_ids)
+            chosen_ids.append(chosen_candidate_ids)
+            rejected_ids.append(rejected_candidate_ids)
+
+        encodings = tokenizer.pad(
+            {
+                "input_ids": [a + b for a, b in zip(prompt_ids, chosen_ids)]
+                + [a + b for a, b in zip(prompt_ids, rejected_ids)]
+            },
+            return_tensors="pt",
+            padding="max_length",
+            max_length=max_length,
         )
-        candidate_max_length = max_length - len(source_ids)
-        chosen_candidate_ids = tokenizer.encode(
-            cand_prefix + pref.chosen_response,
-            max_length=candidate_max_length,
-            truncation=True,
-        )
-        rejected_candidate_ids = tokenizer.encode(
-            cand_prefix + pref.rejected_response,
-            max_length=candidate_max_length,
-            truncation=True,
-        )
+        return encodings
 
-        prompt_ids.append(source_ids)
-        chosen_ids.append(chosen_candidate_ids)
-        rejected_ids.append(rejected_candidate_ids)
+    test_out = collate_fn(data[:4])
+    test_enc = {k: v.to(backbone.device) for k, v in test_out.items()}
+    pdb.set_trace()
 
-    encodings = tokenizer.pad(
-        {
-            "input_ids": [a + b for a, b in zip(prompt_ids, chosen_ids)]
-            + [a + b for a, b in zip(prompt_ids, rejected_ids)]
-        },
-        return_tensors="pt",
-        padding="max_length",
-        max_length=max_length,
-    )
-    return encodings
+    dl = DataLoader(data, batch_size=4, drop_last=False, collate_fn=collate_fn)
+    outputs = []
+    for enc in tqdm(dl):
+        enc = {k: v.to(backbone.device) for k, v in enc.items()}
+        outputs.append(backbone.get_feature(**enc).detach().cpu())
+
+    pd.to_pickle(outputs, out_name)
 
 
-test_out = collate_fn(data[:4])
-test_enc = {k: v.to(backbone.device) for k, v in test_out.items()}
-pdb.set_trace()
-
-dl = DataLoader(data, batch_size=4, drop_last=False, collate_fn=collate_fn)
-outputs = []
-for enc in tqdm(dl):
-    enc = {k: v.to(backbone.device) for k, v in enc.items()}
-    outputs.append(backbone.get_feature(**enc).detach().cpu())
-
-
-pd.to_pickle(outputs, "processed_features_for_rm.pt")
+if __name__ == "__main__":
+    Fire(main)
