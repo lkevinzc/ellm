988d9b77d3bde7af146542bebd5740e72542c2f5
Zichen <lkevinzc@gmail.com>
2024-09-17 10:31:05 +0800

    bf16 for rm server boost qps to ~250 w single GPU card

--- tests/test_rm_server.py
+++ tests/test_rm_server.py
@@ -4,15 +4,16 @@ import pandas as pd
 
 from ellm.oracles.remote.client import RemoteRMOracle
 
-remote_oracle = RemoteRMOracle(remote_rm_url="http://0.0.0.0:8000", max_workers=1)
+n = 50
+remote_oracle = RemoteRMOracle(remote_rm_url="http://0.0.0.0:8000", max_workers=4)
 data = pd.read_json(
-    "/home/aiops/liuzc/ellm/output/apl_ent_pref_certainty_4ep_0910T07:53/eval_results/380.json",
+    "output/neworacle_dpo_offline_0911T19:18/eval_results/380.json",
     orient="records",
     lines=True,
 )
-prompts = data["prompt"]
-candidate_1 = data["response"]
-candidate_2 = data["reference"]
+prompts = data["prompt"][:n]
+candidate_1 = data["response"][:n]
+candidate_2 = data["reference"][:n]
 
 st = time.time()
 result = remote_oracle.compare(prompts, candidate_1, candidate_2, return_probs=True)
